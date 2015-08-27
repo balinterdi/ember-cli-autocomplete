c4a435150da57caa68ee5f9e52085246d45c6195
Balint Erdi <balint.erdi@gmail.com>
2015-08-27 12:08:06 +0200

    Show photo for selected artist

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,11 +1,11 @@
 import Ember from 'ember';
 
 const artists = [
-  { id: 1, name: 'Mike McCready', band: 'Pearl Jam' },
-  { id: 2, name: 'Dave Grohl', band: 'Foo Fighters' },
-  { id: 3, name: 'Josh Homme', band: 'Them Crooked Vultures' },
-  { id: 4, name: 'Jimmy Page', band: 'Led Zeppelin' },
-  { id: 5, name: 'Eddie Vedder', band: 'Pearl Jam' }
+  { id: 1, name: 'Mike McCready', band: 'Pearl Jam', img: 'images/mike-mccready-1.jpg' },
+  { id: 2, name: 'Dave Grohl', band: 'Foo Fighters', img: 'images/dave-grohl.jpg' },
+  { id: 3, name: 'Josh Homme', band: 'Them Crooked Vultures', img: 'images/josh-homme.jpg' },
+  { id: 4, name: 'Jimmy Page', band: 'Led Zeppelin', img: 'images/jimmy-page-1.jpg' },
+  { id: 5, name: 'Eddie Vedder', band: 'Pearl Jam', img: 'images/eddie-vedder.jpg' }
 ];
 
 
