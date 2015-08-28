988d6107782330dc1ee8ff4f098241eb6a27d5af
Balint Erdi <balint.erdi@gmail.com>
2015-08-28 14:12:45 +0200

    Make a decent artist card for the selected artist
    * Also, crop the images so that they are not distorted

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,13 +1,60 @@
 import Ember from 'ember';
 
-const artists = [
-  { id: 1, name: 'Mike McCready', band: 'Pearl Jam', img: 'images/mike-mccready-1.jpg' },
-  { id: 2, name: 'Dave Grohl', band: 'Foo Fighters', img: 'images/dave-grohl.jpg' },
-  { id: 3, name: 'Josh Homme', band: 'Them Crooked Vultures', img: 'images/josh-homme.jpg' },
-  { id: 4, name: 'Jimmy Page', band: 'Led Zeppelin', img: 'images/jimmy-page-1.jpg' },
-  { id: 5, name: 'Eddie Vedder', band: 'Pearl Jam', img: 'images/eddie-vedder.jpg' }
-];
+const Artist = Ember.Object.extend({
+  name: '',
+  bands: null,
+  img: '',
+  bornIn: null,
 
+  age: Ember.computed('bornIn', function() {
+    return new Date().getFullYear() - this.get('bornIn');
+  })
+});
+
+const artists = [
+  Artist.create({
+    id: 1,
+    name: 'Mike McCready',
+    bands: ['Pearl Jam', 'Mad Season'],
+    img: 'images/mike-mccready-1.jpg',
+    bornIn: 1966
+  }),
+  Artist.create({
+    id: 2,
+    name: 'Dave Grohl',
+    bands: ['Foo Fighters', 'Nirvana'],
+    img: 'images/dave-grohl.jpg',
+    bornIn: 1969
+  }),
+  Artist.create({
+    id: 3,
+    name: 'Josh Homme',
+    bands: ['Them Crooked Vultures', 'Queens of the Stone Age'],
+    img: 'images/josh-homme.jpg',
+    bornIn: 1973
+  }),
+  Artist.create({
+    id: 4,
+    name: 'Jimmy Page',
+    bands: ['Led Zeppelin'],
+    img: 'images/jimmy-page-1.jpg',
+    bornIn: 1944
+  }),
+  Artist.create({
+    id: 5,
+    name: 'Eddie Vedder',
+    bands: ['Pearl Jam'],
+    img: 'images/eddie-vedder.jpg',
+    bornIn: 1964
+  }),
+  Artist.create({
+    id: 6,
+    name: 'John Paul Jones',
+    bands: ['Led Zeppelin', 'Them Crooked Vultures'],
+    img: 'images/john-paul-jones.jpg',
+    bornIn: 1946
+  })
+];
 
 export default Ember.Controller.extend({
   selectedArtist: null,
--- tests/dummy/public/image-credits.md
+++ tests/dummy/public/image-credits.md
@@ -3,5 +3,7 @@
 * Josh Homme: https://upload.wikimedia.org/wikipedia/commons/c/c1/Queen_of_the_Stone_Edge-Josh_Homme-IMG_6639.jpg
 * Mike McCready (1): http://images.mstarz.com/data/images/full/10025/pearl-jam-guitarist-mike-mccready-performs-during-a-sold-out-show-at-the-mgm-grand-garden-arena-in-las-vegas-nevada-june-6-2003-the-band-is-touring-in-support-of-its-seventh-studio-album-riot-act.jpg
 * Mike McCready (2): https://commons.wikimedia.org/wiki/File%3AMike_McCready_2009.jpg
-* Eddie Vedder: https://commons.wikimedia.org/wiki/File%3AEddie_Vedder_01.jpg
+* Eddie Vedder (1): https://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/EddieVedder.jpg/220px-EddieVedder.jpg
+* Eddie Vedder (2): https://commons.wikimedia.org/wiki/File%3AEddie_Vedder_01.jpg
 * Dave Grohl: https://commons.wikimedia.org/wiki/File:Dave-Grohl.jpg
+* John Paul Jones: https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/John_Paul_Jones_-_2010.jpg/220px-John_Paul_Jones_-_2010.jpg
