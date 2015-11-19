d909b8ad3e9040482853121ce87c2cddcc84a352
Balint Erdi <balint.erdi@gmail.com>
2015-11-19 10:28:44 +0100

    Add first test case

---
+++ tests/integration/components/auto-complete-test.js
@@ -0,0 +1,96 @@
+import Ember from 'ember';
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+const artists = [
+  Ember.Object.create({ id: 1, name: "Eddie Vedder" }),
+  Ember.Object.create({ id: 2, name: "Dave Grohl" }),
+  Ember.Object.create({ id: 3, name: "John Paul Jones" }),
+  Ember.Object.create({ id: 4, name: "Jimmy Page" })
+];
+
+function typeInInput(text) {
+  this.$('.auto-complete-input')
+    .prop('value', text)
+    .trigger('input');
+}
+
+moduleForComponent('auto-complete', 'Integration | Component | auto complete', {
+  integration: true
+});
+
+test('it works', function(assert) {
+  assert.expect(4);
+
+  // Set any properties with this.set('myProperty', 'value');
+  // Handle any actions with this.on('myAction', function(val) { ... });
+  this.set('selectedArtist', null);
+  this.set('matchingArtists', artists);
+  this.actions = {
+    selectArtist(artist) {
+      this.set('selectedArtist', artist);
+    },
+    filterArtists(searchTerm) {
+      const matchingArtists = artists.filter(function(artist) {
+        searchTerm = searchTerm.toLowerCase();
+        return artist.get('name').toLowerCase().indexOf(searchTerm) === 0;
+      });
+      this.set('matchingArtists', matchingArtists);
+    }
+  };
+
+  this.render(hbs`
+    {{#auto-complete
+        on-select=(action "selectArtist")
+        on-input=(action "filterArtists")
+        as |ac isOpen inputValue toggleDropdown onSelect onInput|}}
+        {{auto-complete-input
+            class="auto-complete-input"
+            autocomplete_=ac
+            value=inputValue
+            on-change=onInput
+            type="text"}}
+        {{#auto-complete-list autocomplete=ac isVisible=isOpen as |list|}}
+          {{#each matchingArtists as |artist|}}
+            {{#auto-complete-option
+                id=artist.id
+                class="auto-complete-option"
+                label=artist.name
+                item=artist
+                list=list
+                on-click=onSelect
+                activeId=selectedArtist.id}}
+              <a href="#">{{artist.name}}</a>
+            {{/auto-complete-option}}
+          {{else}}
+            <li><a href="#">No results.</a></li>
+          {{/each}}
+        {{/auto-complete-list}}
+        {{#auto-complete-dropdown-toggle on-click=toggleDropdown}}
+          <span class="caret"></span>
+        {{/auto-complete-dropdown-toggle}}
+    {{/auto-complete}}
+  `);
+
+  function assertSelectedArtist(name) {
+    assert.equal(this.get('selectedArtist.name'), name);
+  }
+
+  function assertOptionCount(count, message) {
+    assert.equal(this.$('.auto-complete-option').length, count, message);
+  }
+
+  this.$('.auto-complete-option:first').click();
+  assertSelectedArtist.call(this, 'Eddie Vedder');
+
+  this.$('.auto-complete-option:last').click();
+  assertSelectedArtist.call(this, 'Jimmy Page');
+
+  typeInInput.call(this, 'J');
+  assertOptionCount.call(this, 2, "The filtered artists are shown");
+
+  typeInInput.call(this, 'Jo');
+  assertSelectedArtist.call(this, 'John Paul Jones');
+
+  typeInInput.call(this, '');
+});
