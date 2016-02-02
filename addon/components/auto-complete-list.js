6168fa18eab728774faf40d684f99863555de6fd
Balint Erdi <balint.erdi@gmail.com>
2016-02-02 17:40:56 +0100

    No need to show/hide list both ways
    The `isVisible` binding already takes care of it, the `style` attributeBinding is superfluous.

--- addon/components/auto-complete-list.js
+++ addon/components/auto-complete-list.js
@@ -2,10 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   tagName:            'ul',
-  attributeBindings:  ['style'],
   classNames:         'ember-autocomplete-list',
-
-  style: Ember.computed('isVisible', function() {
-    return this.get('isVisible') ? "display:block" : "display:none";
-  })
 });
